c8f65e17bc257dbba9de847e1d3d17412f7046f0
C-o-r-E <can.of.tuna@gmail.com>
2014-03-08 20:33:02 -0500

    adding skeleton for pages

--- ra/main/urls.py
+++ ra/main/urls.py
@@ -4,4 +4,10 @@ from main import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.mainSite, name='mainSite'),
+                       url(r'^about/$', views.about, name='aboutPage'),
+                       url(r'^schedule/$', views.schedule, name='schedulePage'),
+                       url(r'^membership$', views.membership, name='membershipPage'),
+                       url(r'^contact$', views.contact, name='contactPage'),
+#                       url(r'^$', views.mainSite, name='mainSite'),
+#                       url(r'^$', views.mainSite, name='mainSite'),
 )
--- ra/main/views.py
+++ ra/main/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def mainSite(response):
     return render(response, 'main/main.html', {})
+
+def about(response):
+    return render(response, 'main/about.html', {})
+
+def schedule(response):
+    return render(response, 'main/schedule.html', {})
+
+def membership(response):
+    return render(response, 'main/membership.html', {})
+
+def contact(response):
+    return render(response, 'main/contact.html', {})
