5d856919f8d79a7b4777f4b143b81ce6472feb13
C-o-r-E <can.of.tuna@gmail.com>
2014-07-24 13:49:38 -0400

    started working on member views

--- ra/members/views.py
+++ ra/members/views.py
@@ -1,3 +1,11 @@
+from members.models import Member
 from django.shortcuts import render
 
-# Create your views here.
+def members(request):
+    if not request.user.is_authenticated():
+        return render(request, 'main/home.html', {})
+
+    logged_in = True
+    memberList = Member.objects.all()
+
+    return render(request, 'members/members.html', {'member_list': memberList, 'logged_in': logged_in})
