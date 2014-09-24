3b1047c416d197264e007734af5d367bd504c575
C-o-r-E <can.of.tuna@gmail.com>
2014-09-24 19:26:15 -0400

    adding members now works

--- ra/members/views.py
+++ ra/members/views.py
@@ -1,5 +1,9 @@
 from members.models import Member, MemberForm
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+import datetime
+
 
 def members(request):
     if not request.user.is_authenticated():
@@ -14,7 +18,20 @@ def addMember(request):
     if not request.user.is_authenticated():
         return render(request, 'main/home.html', {})
 
+    if request.method == 'POST':
+        memForm = MemberForm(request.POST)
+        if memForm.is_valid():
+            newMember = memForm.save(commit=False)
+            newMember.number = Member.objects.all().count() + 1
+            newMember.first_seen_date = datetime.date.today()
+            newMember.last_seen_date = datetime.date.today()
+            newMember.save()
+            return HttpResponseRedirect('../')
+
+    else:
+        memForm = MemberForm()
+
     logged_in = True
-    memForm = MemberForm()
+    
     
     return render(request, 'members/add.html', {'mem_form': memForm, 'logged_in': logged_in})
